d91a75a8028a919a4ee969fe97de7b2335e37d21
fabiwan <fabiwan@55ead531-2e2b-0410-b405-93e52e755431>
2009-11-07 17:18:47 +0000

    List of patches that have been applied to the Mozilla 1.8.1 core.
    git-svn-id: http://svn.code.sf.net/p/kompozer/svn/trunk@188 55ead531-2e2b-0410-b405-93e52e755431

---
+++ patches/debian/remove-nonfree.sh
@@ -0,0 +1,161 @@
+#! /bin/sh
+
+cd mozilla/
+svn rm ./build/wince/shunt/build/shunt.vcb
+svn rm ./build/wince/shunt/build/shunt.vco
+svn rm ./directory/c-sdk/ldap/examples/macintosh/*.u
+svn rm ./directory/c-sdk/ldap/libraries/macintosh/LDAPClient.mcp
+svn rm ./modules/oji/tests/script/killer.exe
+svn rm ./modules/plugin/samples/default/mac/NullPlugin.rsrc
+svn rm "./modules/plugin/tools/sdk/samples/basic/mac/Basic Plugin.mcp"
+svn rm ./modules/plugin/tools/sdk/samples/basic/mac/Basic.rsrc
+svn rm "./modules/plugin/tools/sdk/samples/scriptable/mac/Scriptable Plugin.mcp"
+svn rm "./modules/plugin/tools/sdk/samples/scriptable/mac/Scriptable PluginIDL.mcp"
+svn rm ./modules/plugin/tools/sdk/samples/scriptable/mac/Scriptable.rsrc
+svn rm ./other-licenses/7zstub/firefox/7zSD.sfx
+svn rm ./toolkit/mozapps/update/src/updater/macbuild/Contents/Resources/English.lproj/MainMenu.nib/keyedobjects.nib
+svn rm ./toolkit/mozapps/update/src/updater/macbuild/Contents/PkgInfo
+svn rm ./extensions/manticore/resources/manticore.psd
+svn rm ./extensions/universalchardet/doc/UniversalCharsetDetection.doc
+svn rm ./config/bin2rc.exe
+svn rm ./config/makedep.exe
+svn rm ./config/mangle.exe
+svn rm ./config/mantomak.exe
+svn rm ./embedding/browser/activex/src/pluginhostctrl/cab/redist/ATL.DLL
+svn rm ./embedding/browser/activex/tests/vbrowse/browser.frx
+svn rm ./embedding/browser/activex/tests/vbrowse/frmToolBar.frx
+svn rm ./embedding/browser/cocoa/res/English.lproj/alert.nib/objects.nib
+svn rm ./embedding/browser/powerplant/resources/cfmAlias.rsrc
+svn rm ./embedding/browser/powerplant/resources/cfmAliasDebug.rsrc
+svn rm ./embedding/minimo/mozconfig
+svn rm ./embedding/qa/testembed/testembed.aps
+svn rm ./embedding/tests/MSDotNETCSEmbed/MSDotNETCSEmbed.suo
+svn rm ./embedding/tests/cocoaEmbed/res/English.lproj/MainMenu.nib/objects.nib
+svn rm ./embedding/wrappers/DotNETEmbed/DotNETEmbed.snk
+svn rm ./intl/unicharutil/tools/data/case.dat
+svn rm ./intl/unicharutil/tools/data/cmbcl.dat
+svn rm ./intl/unicharutil/tools/data/ctype.dat
+svn rm ./intl/unicharutil/tools/data/decomp.dat
+svn rm ./intl/unicharutil/tools/data/num.dat
+svn rm ./js/src/fdlibm/fdlibm.mdp
+svn rm ./js/src/js.mdp
+svn rm ./js/src/liveconnect/jsj_nodl.c
+svn rm ./plugin/oji/JEP/MRJPlugin.plugin/Contents/MacOS/MRJPlugin
+svn rm ./plugin/oji/JEP/MRJPlugin.plugin/Contents/MacOS/MRJPlugin.jar
+svn rm ./plugin/oji/JEP/MRJPlugin.plugin/Contents/Resources/MRJPlugin.rsrc
+svn rm ./plugin/oji/JEP/JavaEmbeddingPlugin.bundle/Contents/MacOS/JavaEmbeddingPlugin
+svn rm ./plugin/oji/JEP/JavaEmbeddingPlugin.bundle/Contents/Resources/Java/JavaEmbeddingPlugin.jar
+svn rm ./plugin/oji/MRJ/plugin/Resources/Dialogs.rsrc
+svn rm ./plugin/oji/MRJ/plugin/Resources/Strings.rsrc
+svn rm ./plugin/oji/MRJ/plugin/Resources/Version.rsrc
+svn rm ./plugin/oji/MRJ/plugin/MRJPlugin.jar
+svn rm ./plugin/oji/MRJ/plugin/netscape.plugin.jar
+svn rm ./plugin/oji/MRJ/plugin/Source/JMURLConnection
+svn rm ./plugin/oji/MRJ/testing/ConsoleApplet/ConsoleApplet.mcp
+svn rm ./plugin/oji/MRJ/testing/JSApplet/JSApplet.mcp
+svn rm ./plugin/oji/MRJ/testing/SwingApplet/TestApplet.mcp
+svn rm ./plugin/oji/MRJ/testing/TrivialApplet/MyApplet.mcp
+svn rm ./plugin/oji/MRJCarbon/MRJSDK/JavaFrameworks/JavaEmbeddingLib
+svn rm ./plugin/oji/MRJCarbon/plugin/MRJPlugin.jar
+svn rm ./plugin/oji/MRJCarbon/plugin/Resources/Dialogs.rsrc
+svn rm ./plugin/oji/MRJCarbon/plugin/Resources/Strings.rsrc
+svn rm ./plugin/oji/MRJCarbon/plugin/Resources/Version.rsrc
+svn rm ./xpcom/reflect/xptcall/tests/eVC4/XPTCInvoke_Testing.cpp
+svn rm ./xpcom/tests/StringFactoringTests/StringTest.mcp
+svn rm ./xpcom/typelib/xpidl/macplugin/compiler.rsrc
+svn rm ./xpcom/typelib/xpidl/macplugin/linker.rsrc
+svn rm ./xpcom/typelib/xpidl/macplugin/panel.rsrc
+svn rm ./xpcom/typelib/xpidl/macplugin/version.rsrc
+svn rm ./xpinstall/packager/mac/ASEncoder/build/ASEncoder.mcp
+svn rm ./xpinstall/packager/mac/ASEncoder/rsrc/ASEncoder.rsrc
+svn rm ./xpinstall/packager/mac/ASEncoder/src/ASEncoderUsage.script
+svn rm ./xpinstall/test/pre_checkin.xpi
+svn rm ./xpinstall/wizard/libxpnet/GUSI/lib/GUSI_Core.PPC.Lib
+svn rm ./xpinstall/wizard/libxpnet/GUSI/lib/GUSI_ForeignThreads.PPC.Lib
+svn rm ./xpinstall/wizard/libxpnet/GUSI/lib/GUSI_MPW.PPC.Lib
+svn rm ./xpinstall/wizard/libxpnet/GUSI/lib/GUSI_MSL.PPC.Lib
+svn rm ./xpinstall/wizard/libxpnet/GUSI/lib/GUSI_SIOUX.PPC.Lib
+svn rm ./xpinstall/wizard/libxpnet/GUSI/lib/GUSI_Sfio.PPC.Lib
+svn rm ./xpinstall/wizard/mac/rsrc/MIWCommon.rsrc
+svn rm ./xpinstall/wizard/mac/rsrc/Mozilla.rsrc
+svn rm ./xpinstall/wizard/os2/setuprsc/box_ch_d.bmp
+svn rm ./xpinstall/wizard/os2/setuprsc/box_chec.bmp
+svn rm ./xpinstall/wizard/os2/setuprsc/box_unch.bmp
+svn rm ./gfx/cairo/libpixman/src/pixman-vc71.lib
+svn rm ./gfx/src/mac/printerplugin/Resources/English.lproj/Localizable.strings
+svn rm ./gfx/src/mac/nsMacGFX.rsrc
+svn rm ./layout/doc/SpaceMgr_BlockReflSt_OD.sda
+svn rm ./layout/doc/object_diagram_template.sda
+svn rm ./layout/html/tests/block/bugs/RealSnow.jar
+svn rm ./webshell/tests/viewer/mac/cfmAlias.rsrc
+svn rm ./webshell/tests/viewer/mac/cfmAliasDebug.rsrc
+svn rm ./webshell/tests/viewer/mac/viewer.ppob
+svn rm ./webshell/tests/viewer/mac/viewer.rsrc
+svn rm ./webshell/tests/viewer/mac/viewercarb.rsrc
+svn rm ./webshell/tests/viewer/viewer-beos.rsrc
+svn rm ./xpfe/bootstrap/appleevents/nsAppleEvents.rsrc
+svn rm ./xpfe/bootstrap/apprunner-beos.rsrc
+svn rm ./mailnews/mapi/old/tests/mapitest/mapitest.ncb
+svn rm ./mailnews/mapi/old/tests/mapitest/mapitest.opt
+svn rm ./toolkit/mozapps/installer/windows/nsis/Processes.dll
+svn rm ./toolkit/mozapps/installer/windows/nsis/ShellLink.dll
+svn rm ./toolkit/mozapps/installer/windows/nsis/nsProcess.dll
+svn rm ./mailnews/addrbook/src/nsAbOSXCard.o
+svn rm ./mailnews/addrbook/src/nsAbOSXDirFactory.o
+svn rm ./mailnews/addrbook/src/nsAbOSXDirectory.o
+svn rm ./mailnews/addrbook/src/nsAbOSXUtils.o
+
+echo also we need to remove any copyrighted image files from the mozilla src  suite,  not replaced by the branding process. This includes theme throbbers depending on how you handle that and windows icons, app icons, windows splash etc
+
+svn rm ./xpfe/bootstrap/splash.bmp
+svn rm ./xpfe/bootstrap/mozilla.ico
+svn rm ./xpfe/bootstrap/mozos2.ico
+svn rm ./browser/app/mozicon128.png
+svn rm ./browser/app/mozicon16.xpm
+svn rm ./browser/app/mozicon50.xpm
+
+# I'm not sure about the os2 as I can't see them
+svn rm ./xpfe/bootstrap/icons/os2/mozilla.ico
+
+svn rm ./xpinstall/wizard/windows/setuprsc/bitmap1.bmp
+svn rm ./xpinstall/wizard/windows/setuprsc/downloadLogo.bmp
+svn rm ./xpinstall/wizard/windows/setuprsc/turbo-systray.bmp
+
+svn rm ./xpinstall/wizard/os2/nsinstall/stubinstall.ico
+svn rm ./xpinstall/wizard/os2/setuprsc/downloadLogo.bmp
+svn rm ./xpinstall/wizard/os2/setuprsc/bitmap1.bmp
+
+svn rm ./suite/branding/icons/gtk/seamonkey.png # 128x128
+svn rm ./suite/branding/icons/gtk/default.xpm # 32x32
+svn rm ./suite/branding/icons/gtk/default16.xpm #16x16
+svn rm ./suite/branding/content/logo.gif # 200x200
+
+# remove the throbbers as they are branded
+
+svn rm ./themes/classic/communicator/brand/throbber-single.gif
+svn rm ./themes/classic/communicator/brand/throbber16-single.gif
+svn rm ./themes/classic/communicator/brand/throbber-anim.gif
+svn rm ./themes/classic/communicator/brand/throbber16-anim.gif
+
+svn rm ./themes/modern/communicator/brand/throbber-single.gif
+svn rm ./themes/modern/communicator/brand/throbber16-single.gif
+svn rm ./themes/modern/communicator/brand/throbber-anim.gif
+svn rm ./themes/modern/communicator/brand/throbber16-anim.gif
+
+svn rm ./xpfe/bootstrap/splash.xpm
+svn rm ./themes/classic/communicator/icons/turbo-systray.gif
+svn rm ./themes/modern/communicator/icons/turbo-systray.gif
+
+
+svn rm -r other-licenses/
+svn rm README/mozilla/README.os2
+
+svn rm netwerk/protocol/ftp/doc/rfc959.txt
+svn rm directory/c-sdk/ldap/docs/draft-ietf-ldapext-ldap-c-api-05.txt
+
+# sunbird stuff
+
+svn rm calendar/sunbird/app/sunbird.ico
+svn rm calendar/sunbird/app/default.xpm
+svn rm calendar/resources/content/sunbird_small.png
+
